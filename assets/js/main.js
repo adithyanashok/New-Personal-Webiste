f26c46245e1f88f693fe68bcc9c22a0f2df23c88
Adithyan Ashok <adithyanashok406@gmail.com>
2022-03-12 13:44:47 +0530

    js file updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,13 @@
   const on = (type, el, listener, all = false) => {
      let selectEl = select(el, all)
 
-    // if (selectEl) {
-    //   if (all) {
-    //     selectEl.forEach(e => e.addEventListener(type, listener))
-    //   } else {
-    //     selectEl.addEventListener(type, listener)
-    //   }
-    // }
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
   }
 
   /**
